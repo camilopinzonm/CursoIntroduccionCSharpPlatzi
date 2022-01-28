a797a858777beefd01282d058ed40cfdc561a15a
camilopinzonm <pinzomps@gmail.com>
2022-01-28 12:54:47 -0500

    Primer commit del curso

---
+++ CursoIntroduccionCSharp/CursoIntroduccionCSharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CursoIntroduccionCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Platzi!");
+
+            int height = 175;
+            int age = 38;
+            string name = "Camilo Pinzon";
+            string information = "the requested info is: \n User name " + name + "\n user age "
+                + age + " \n user height " + height;
+            Console.WriteLine(information);
+        }
+    }
+}
